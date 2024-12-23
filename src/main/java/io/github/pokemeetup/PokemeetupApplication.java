977dc4e98c2dd3f7a2ab52b8ad7fd2660a850b5c
Greafull <micmicgames21@gmail.com>
2024-12-23 11:36:40 -0500

    set max fps to monitor refresh rate and made default mode borderless window

--- src/main/java/io/github/pokemeetup/PokemeetupApplication.java
+++ src/main/java/io/github/pokemeetup/PokemeetupApplication.java
@@ -3,11 +3,14 @@
 	import com.badlogic.gdx.Game;
 	import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 	import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+	import com.badlogic.gdx.backends.lwjgl3.Lwjgl3WindowConfiguration;
 	import lombok.Getter;
 	import org.springframework.boot.SpringApplication;
 	import org.springframework.boot.autoconfigure.SpringBootApplication;
 	import org.springframework.context.ApplicationContext;
 	import org.springframework.context.annotation.ComponentScan;
+
+	import java.awt.*;
 
 	@SpringBootApplication
 	@ComponentScan("io.github.pokemeetup")
@@ -17,14 +20,21 @@
 		private static ApplicationContext springContext;
 
 		public static void main(String[] args) {
+			System.setProperty("java.awt.headless", "false");
 			springContext = SpringApplication.run(PokemeetupApplication.class, args);
+
+			DisplayMode monitor = GraphicsEnvironment.getLocalGraphicsEnvironment().
+					getDefaultScreenDevice().getDisplayMode();
 
 			Game game = springContext.getBean(Game.class);
 
 			Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 			config.setTitle("PokeMeetup");
-			config.setWindowedMode(1280, 720);
-			config.setForegroundFPS(60);
+			config.setWindowedMode(monitor.getWidth(), monitor.getHeight());
+			//gives borderless window effect
+			config.setDecorated(false);
+
+			config.setForegroundFPS(monitor.getRefreshRate());
 			config.useVsync(true);
 
 			new Lwjgl3Application(game, config);
