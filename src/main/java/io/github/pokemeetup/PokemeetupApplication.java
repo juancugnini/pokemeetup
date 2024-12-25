6eeebebf306b9af630068b7ad66c5926971544eb
Greafull <micmicgames21@gmail.com>
2024-12-25 09:32:47 -0500

    set max fps to monitor refresh rate and made default mode borderless window, fixied indentation issue

--- src/main/java/io/github/pokemeetup/PokemeetupApplication.java
+++ src/main/java/io/github/pokemeetup/PokemeetupApplication.java
@@ -1,42 +1,41 @@
-	package io.github.pokemeetup;
+package io.github.pokemeetup;
 
-	import com.badlogic.gdx.Game;
-	import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
-	import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
-	import com.badlogic.gdx.backends.lwjgl3.Lwjgl3WindowConfiguration;
-	import lombok.Getter;
-	import org.springframework.boot.SpringApplication;
-	import org.springframework.boot.autoconfigure.SpringBootApplication;
-	import org.springframework.context.ApplicationContext;
-	import org.springframework.context.annotation.ComponentScan;
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import lombok.Getter;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
 
-	import java.awt.*;
+import java.awt.*;
 
-	@SpringBootApplication
-	@ComponentScan("io.github.pokemeetup")
-	public class PokemeetupApplication {
+@SpringBootApplication
+@ComponentScan("io.github.pokemeetup")
+public class PokemeetupApplication {
 
-		@Getter
-		private static ApplicationContext springContext;
+	@Getter
+	private static ApplicationContext springContext;
 
-		public static void main(String[] args) {
-			System.setProperty("java.awt.headless", "false");
-			springContext = SpringApplication.run(PokemeetupApplication.class, args);
+	public static void main(String[] args) {
+		System.setProperty("java.awt.headless", "false");
+		springContext = SpringApplication.run(PokemeetupApplication.class, args);
 
-			DisplayMode monitor = GraphicsEnvironment.getLocalGraphicsEnvironment().
+		DisplayMode monitor = GraphicsEnvironment.getLocalGraphicsEnvironment().
 					getDefaultScreenDevice().getDisplayMode();
 
-			Game game = springContext.getBean(Game.class);
+		Game game = springContext.getBean(Game.class);
 
-			Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
-			config.setTitle("PokeMeetup");
-			config.setWindowedMode(monitor.getWidth(), monitor.getHeight());
-			//gives borderless window effect
-			config.setDecorated(false);
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setTitle("PokeMeetup");
+		config.setWindowedMode(monitor.getWidth(), monitor.getHeight());
+		//gives borderless window effect
+		config.setDecorated(false);
 
-			config.setForegroundFPS(monitor.getRefreshRate());
-			config.useVsync(true);
+		config.setForegroundFPS(monitor.getRefreshRate());
+		config.useVsync(true);
 
-			new Lwjgl3Application(game, config);
-		}
+		new Lwjgl3Application(game, config);
 	}
+}
